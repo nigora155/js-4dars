60ca9e73c51d74bdadaeb18ab7724846597bf6e6
nigora155 <nigoradjumashevs@gmail.com>
2025-04-07 13:08:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let random = alert("щас у вас на экране появится рандомное");
+
+let randomRaqam = Math.round(Math.random() * 400);
+alert("Random son: " + randomRaqam);
+
+let malumot = alert("щас мы должны умножить число которое появилось на экране на 2 и разделитьна 3");
+
+let kopaytirish = randomRaqam * 2
+
+let bolish = randomRaqam / 2
+
+let foiz = randomRaqam % 2
+
+let natija = alert("полученный ответ когда мы умножили на 2 : " + kopaytirish + "\n"
+    + "полученный ответ когда мы разделили число на 2 : " + bolish + "\n"
+    + "рандом число когда мы разделили его на 3 процент : " + foiz
+)
